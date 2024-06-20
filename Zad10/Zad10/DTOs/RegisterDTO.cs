68328d101193a395c110ddc71d8a380a1d237952
Somsiad <s25160@pjwstk.edu.pl>
2024-06-20 14:09:15 +0200

    done

---
+++ Zad10/Zad10/DTOs/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zad10.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; }
+}
---
+++ Zad10/Zad10/Properties/Extension/Exception.cs
@@ -0,0 +1,15 @@
+﻿namespace Zad10.Properties.Extension;
+
+public static class Exception
+{
+    public static void ConfigureExceptionHandler(this IApplicationBuilder appBuilder)
+    {
+        appBuilder.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                // Implement exception handling here
+            });
+        });
+    }
+}
